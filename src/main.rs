a9a9151acb851d8c20f1fa9d2ceceb6e4916ecbd
ef2m1y <shisotem01072002@gmail.com>
2023-01-31 05:17:27 +0900

    impl input value validation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::io;
+use kakeibo_cli_app::services;
 
 fn main() {
     let mut service_type = String::new();
     println!("[実行] 0:登録 or 1:集計");
     io::stdin().read_line(&mut service_type).unwrap();
     let service_type: u8 = service_type.trim().parse().expect("数値で入力して下さい");
-    // TODO: 入力値のバリデーション
+    services::validate::InputValidator::validate_service_type(service_type);
 
     if service_type == 0 {
         println!("登録機能");
